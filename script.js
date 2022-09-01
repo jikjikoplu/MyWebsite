e7e83d198eae318c3277de08ced90daf6be313d6
LR <107756767+jikjikoplu@users.noreply.github.com>
2022-09-01 10:18:09 -0400

    Update 2.1.1

--- script.js
+++ script.js
@@ -1,57 +1,95 @@
-// Variables
-var nameList = ["python", "java", "javascript", "html", "css"],
-    locList = [5732, 1201, 2458, 432, 302],
-    projectsList = [5, 2, 6, 2, 2],
-    colorsList = ["#0000e6", "#cc0000", "#ffff00", "#e65c00", "#660066"];
-var widthMax = 400,
-    locMax = Math.max(...locList),
-    projectsMax = Math.max(...projectsList);
-var locTotal = 0, projectsTotal = 13;
-var myWidth, myLoc, myProjects, myPercent, myName, myColor;
+// VARIABLES
+// Colors
+var pythonBlue = "#0000e6",
+    javaRed = "#cc0000",
+    javascriptYellow = "#ffff00",
+    htmlOrange = "#e65c00",
+    cssPurple = "#660066";
+
+// Section 1
+var nameList = ["Python", "Java", "JavaScript", "HTML", "CSS",
+                "Python", "Java", "JavaScript", "HTML", "CSS"],
+    valueList = [5732, 1201, 2484, 411, 312, 5, 2, 6, 2, 2],
+    myPos,myName, myValue, myColor, myPercent, myWidth, myType,
+    widthMax = 400, valueMax, totalLoc = 0, totalProjects = 13,
+    runLength = valueList.length, graphBar, graphBarText;
+
+// Section 2
 var projectNameList = ["Kingdom of War v1.2", "Kingdom of War - Multiplayer",
                        "LimeBot", "FarmBot", "MapGen 4", "Exorcist",
                        "The Random Trivia Game", "Pong", "Racing Game 2",
                        "Battlefield Simulator", "30 Second Racing Game",
                        "The Devil in Me", "My Website"],
-    projectLocList = [1845, 2561, 74, 783, 469, 876, 325, 167, 197, 1268, 94,
-                      694, 832],
-    projectColorList = ["py", "py", "py", "py", "py", "j", "j", "hc", "js", "js", "js", "js", "hc"];
-var maxProjectLoc = Math.max(...projectLocList);
-var myPos, myProject;
-var showLinks = false;
-var altLinkIDList = ["alt-link-pong", "alt-link-rg2-1", "alt-link-rg2-2",
+    projectLocList = [1845, 2561, 85, 794, 469, 876, 325, 167, 197, 1268, 94,
+                      694, 787],
+    projectColorList = ["py", "py", "py", "py", "py", "j", "j", "hc", "js",
+                        "js", "js", "js", "hc"],
+    maxProjectLoc = Math.max(...projectLocList), myProject;
+
+//Section Three
+var version = "2.1.1";
+
+// Section Four
+var showLinks = false,
+    altLinkIDList = ["alt-link-pong", "alt-link-rg2-1", "alt-link-rg2-2",
                      "alt-link-basim", "alt-link-3srg", "alt-link-tdim"];
 
-// Graph Bar Width Calculation
-for(var i = 0; i < locList.length; i++){
-    myName = nameList[i];
-    myColor = colorsList[i];
-    // LoC
-    myLoc = locList[i];
-    myPercent = myLoc / locMax;
-    myWidth = myPercent * widthMax;
-    document.getElementById("loc-bar-" + myName).style.width = myWidth + "px";
-    document.getElementById("loc-bar-" + myName + "-text").innerHTML +=
-        myLoc + " Lines of Code (" + Math.round(myPercent * 100) + "%)";
-    locTotal += myLoc;
-    document.getElementById("loc-bar-" + myName).style.backgroundColor =
-        myColor;
-    // Projects
-    myProjects = projectsList[i];
-    myPercent = myProjects / projectsMax;
-    myWidth = myPercent * widthMax;
-    document.getElementById("projects-bar-" + myName).style.width =
-    myWidth + "px";
-    document.getElementById("projects-bar-" + myName + "-text").innerHTML +=
-        myProjects + " Projects (" + Math.round(myPercent * 100) + "%)";
-    document.getElementById("projects-bar-" + myName).style.backgroundColor =
-        myColor;
-}
-document.getElementById("total-loc").innerHTML = "Total: " + locTotal;
-document.getElementById("total-projects").innerHTML =
-    "Total: " + projectsTotal;
 
-// Project Comparision Bar Widths
+// CODE
+// Section 1: Graphs 1 & 2 Widths
+for(var i = 0; i < runLength / 2; i++){
+    totalLoc += valueList[i];
+}
+document.getElementById("total-loc").innerHTML =
+    "Total: " + totalLoc;
+document.getElementById("total-projects").innerHTML =
+    "Total: " + totalProjects;
+for(i = 0; i < runLength; i++){
+    if(i == 0){
+        valueMax = Math.max(...valueList);
+        myType = " Lines of Code ";
+    }else if(i == 5){
+        valueMax = Math.max(...valueList);
+        myType = " Projects ";
+    }
+    myPos = valueList.indexOf(Math.max(...valueList));
+    myValue = valueList[myPos];
+    myName = nameList[myPos];
+    valueList.splice(myPos, 1);
+    nameList.splice(myPos, 1);
+    //alert(myPos + " / " + myValue + " / " + myName);
+    switch(myName){
+        case "Python":
+            myColor = pythonBlue;
+            break;
+        case "Java":
+            myColor = javaRed;
+            break;
+        case "JavaScript":
+            myColor = javascriptYellow;
+            break;
+        case "HTML":
+            myColor = htmlOrange;
+            break;
+        case "CSS":
+            myColor = cssPurple;
+            break;
+    }
+    myPercent = myValue / valueMax;
+    myWidth = myPercent * widthMax;
+    document.getElementsByClassName("graph-bar")[i].style.width =
+        myWidth + "px";
+    document.getElementsByClassName("graph-bar")[i].style.backgroundColor =
+        myColor;
+    document.getElementsByClassName("graph-bar-text")[i].innerHTML =
+        myName + " - " + myValue + myType +
+        "(" + Math.round(myPercent * 100) + "%)";
+}
+
+// Section 3: Website Version
+document.getElementById("title-section-version").innerHTML = version;
+
+// Section 2: Graph 3 Widths
 for(i = 0; i < 10; i++){
     myPos = projectLocList.indexOf(Math.max(...projectLocList));
     myProject = projectNameList[myPos];
@@ -59,16 +97,16 @@ for(i = 0; i < 10; i++){
     myColor = projectColorList[myPos];
     switch(myColor){
         case "py":
-            myColor = "#0000e6";
+            myColor = pythonBlue;
             break;
         case "j":
-            myColor = "#cc0000";
+            myColor = javaRed;
             break;
         case "js":
-            myColor = "#ffff00";
+            myColor = javascriptYellow;
             break;
         case "hc":
-            myColor = "#e65c00";
+            myColor = htmlOrange;
             break;
     }
     myWidth = (myLoc / maxProjectLoc) * widthMax;
@@ -83,7 +121,7 @@ for(i = 0; i < 10; i++){
         style.backgroundColor = myColor;
 }
 
-// Toggle Links Button
+// Section 4: Toggle Links Button
 function toggleLinks(){
     if(! showLinks){
         showLinks = true;
