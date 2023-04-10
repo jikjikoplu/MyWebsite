e1c0ad1c267363d85eccc137dc015957c85e56fc
LR <107756767+jikjikoplu@users.noreply.github.com>
2023-04-10 08:23:24 -0400

    Update v2.11.6

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ var projectNameList = ["MapGen 5", "FakeOS", "My Website", "Pong",
     totalProjects = projectNameList.length;
 
 // Section Five
-var version = "v2.11.5";
+var version = "v2.11.6";
 
 // Section Six
 var showLinks = true,
